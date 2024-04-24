345ca0f8310bb2904d4a7cc18abbd8ec15c9e921
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-04-24 02:07:10 +0200

    module 4 file

--- js/module-4.js
+++ js/module-4.js
@@ -135,3 +135,75 @@
 // // /==========================================================================
 // ================================================================
 // // ==================================================================
+// Напиши функцію getAllPropValues(propName),
+//     яка приймає один параметр propName - ім'я (ключ) властивості.
+//     Функція повинна повернути масив усіх значень властивості
+//     з таким ім'ям з кожного об'єкта в масиві products.
+//     Якщо в об'єктах відсутні властивості з таким ім'ям,
+//     функція повинна повернути порожній масив.
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   const finishName = [];
+
+//   for (const product of products) {
+//     if (propName in product) {
+//       finishName.push(product[propName]);
+//     }
+//   }
+//   return finishName;
+// }
+// // ================================================================
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   const finishName = [];
+
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       finishName.push(product[propName]);
+//     }
+//   }
+//   return finishName;
+// }
+// // =====================================
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   const finishName = [];
+
+//   for (const product of products) {
+//     switch (propName) {
+//       case 'name':
+//         finishName.push(product.name);
+//         break;
+
+//       case 'price':
+//         finishName.push(product.price);
+//         break;
+
+//       case 'quantity':
+//         finishName.push(product.quantity);
+//         break;
+//     }
+//   }
+//   return finishName;
+// }
+
+// console.log(getAllPropValues('name')); //["Radar", "Scanner", "Droid", "Grip"]
+// console.log(getAllPropValues('quantity')); //[4, 3, 7, 9]
+// console.log(getAllPropValues('category')); //[]
